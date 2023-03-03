e12fd15b6167864242427b35b5dc023db559b4ed
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-04 05:06:26 +0800

    Created class stack_count score to count total score using stack then set initial value of it

--- guessnumberproj.py
+++ guessnumberproj.py
@@ -1,1 +1,13 @@
 import random as rd
+
+############# DSA's functions #############
+class stack_count_score():
+
+    def __init__(self,current_number,name,picked_number):
+        self.name = name
+        self.current_number = current_number
+        self.picked_number = picked_number
+        self.stack_ = []
+        self.set_used = {}
+        self.count_hit = 0
+        self.count_blow = 0
