b3b8dde3967045ea18958828a54f2461fd5eaa95
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-04 06:09:15 +0800

    Checks if the middle value at the middle is less than search value then apply the function again button

--- guessnumberproj.py
+++ guessnumberproj.py
@@ -99,3 +99,5 @@ def binarySearch(arr, l, r, x):
             return mid
         elif arr[mid] > x:
             return binarySearch(arr, l, mid-1, x)
+        else:
+            return binarySearch(arr, mid + 1, r, x)     
